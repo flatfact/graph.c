6e2d8849e917a0a1c55b93945c4da5b367429324
Flatfact.com <trailersubthai@gmail.com>
2015-07-15 22:23:39 +0700

    Create graph.c

---
+++ graph.c
@@ -0,0 +1,26 @@
+// (c) 2015 Subthai_Production , Flatfact.com all right reserved.
+// All of the contents are material of Subthai_Production and its related.
+//Feel free to use for ya'll homework :)
+//v.1.2 alpha Latest update in 15/7/2015
+#include <stdio.h>
+main() {
+       int x1,x2,y1,y2,a,d;
+       float slope=0.0f;
+       float b=0.0f;
+       printf("x1 = ");
+       scanf("%d",&x1);
+       printf("y1 = ");
+       scanf("%d",&y1);
+       printf("x2 = ");
+       scanf("%d",&x2);
+       printf("y2 = ");
+       scanf("%d",&y2);
+       printf("Calculating Equation of (%d,%d) (%d,%d) ...",x1,y1,x2,y2);
+       a=(y1-y2);
+       d=(x1-x2);
+       slope=(float)a/(float)d;
+       b = y1-(slope*x1);
+       printf("\ny = (%.3f)x + (%.3f)",slope,b);
+       printf("\nSlope = %d/%d or %.3f",a,d,slope);
+       getch();
+}
