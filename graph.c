764e3a71f4f1234909f8d3b00d80889e94568f7b
Flatfact.com <trailersubthai@gmail.com>
2015-07-15 22:36:11 +0700

    Update graph.c

--- graph.c
+++ graph.c
@@ -1,7 +1,7 @@
 // (c) 2015 Subthai_Production , Flatfact.com all right reserved.
 // All of the contents are material of Subthai_Production and its related.
 //Feel free to use for ya'll homework :)
-//v.1.2 alpha Latest update in 15/7/2015
+//v.1.3 beta Latest update in 15/7/2558
 #include <stdio.h>
 main() {
        int x1,x2,y1,y2,a,d;
@@ -16,11 +16,14 @@ main() {
        printf("y2 = ");
        scanf("%d",&y2);
        printf("Calculating Equation of (%d,%d) (%d,%d) ...",x1,y1,x2,y2);
+       printf("\n-----------Answer-----------");
        a=(y1-y2);
        d=(x1-x2);
        slope=(float)a/(float)d;
        b = y1-(slope*x1);
        printf("\ny = (%.3f)x + (%.3f)",slope,b);
+       printf("\ny = %d/%d x + %.3f",a,d,b);
        printf("\nSlope = %d/%d or %.3f",a,d,slope);
+       printf("\nb = %d-(%.2f*%d) = %.3f",y1,slope,x1,b);
        getch();
 }
